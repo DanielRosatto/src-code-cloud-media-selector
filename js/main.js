005fb94bf7c3dd9a611b8742a40a6a273dc94db3
drosatto <danielrosatto@gmail.com>
2021-08-18 22:38:03 -0300

    Se corrige main

--- js/main.js
+++ js/main.js
@@ -10,4 +10,5 @@
 $(document).ready(function main(){
     share= new TextValider("https://api.themoviedb.org/3",//url
         "html body section#cont1.col-12 div#enc1.col-12 div#imgEml.col-5");
-);
+      }
+  );
