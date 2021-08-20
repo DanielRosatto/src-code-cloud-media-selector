155c365c37cb59f899605200b88550a810d934c0
drosatto <danielrosatto@gmail.com>
2021-08-20 10:39:05 -0300

    seprueba con jQuery en main

--- js/main.js
+++ js/main.js
@@ -8,8 +8,6 @@
   * Archivo main.js                                                                                     *
   **********************************************************************************************************/
 $(document).ready(function main(){
-    share= new TextValider("www.w3c.org",//url
-        "html body section#cont1.col-12 div#enc1.col-12 div#imgEml.col-5");
-    refreshDiv;
+      $("div#resultado").replaceWith("<h1>Hello, World!</h1>");
     }
   );
