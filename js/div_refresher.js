6fc9a1730c266301b51b9e66eb8338bb946c35d9
drosatto <danielrosatto@gmail.com>
2021-08-20 08:59:36 -0300

    pri r er intento de video incrustado

---
+++ js/div_refresher.js
@@ -0,0 +1,4 @@
+function refreshDiv(html) {
+    var div = document.getElementById('resultado');
+    div.innerHTML = html;
+  }
---
+++ js/providerMedia4.js
@@ -0,0 +1,4 @@
+function getVideo4() {
+    var html = HtmlService.createHtmlOutputFromFile('../templates/Video4.html').getContent();
+    return html
+  }
