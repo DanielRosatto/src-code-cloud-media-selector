e917f5bba9c8b6eca9e86b28a7982cf546ff9582
drosatto <danielrosatto@gmail.com>
2021-08-22 19:58:06 -0300

    prueba de video 5

--- js/providerMedia.js
+++ js/providerMedia.js
@@ -1,13 +1,11 @@
 class ProviderMedia{
     constructor(){
         this.media_content="\
-            <div id='resultado'>\
-            <p><strong>Reproduciendo el Vídeo predeterminado...</strong></p>\
-            <iframe src= \
-                'https://drive.google.com/file/d/1b4A0x3maDQ46Dv3MQO1fGz2NBokK9Apw/preview' \
+            <video\
                 width= '600' \
                 height= '480' >\
-            </iframe>\
+                <source src='https://doc-0c-0o-docs.googleusercontent.com/docs/securesc/orrltrp800cs9if6vhbeor0eujih4om2/rv2v2l2gcm4q6498grr9he5784p7c5d0/1629672450000/17705489693793960751/17705489693793960751/1b4A0x3maDQ46Dv3MQO1fGz2NBokK9Apw?e=download&authuser=4&nonce=bp0d9eu5t4mv4&user=17705489693793960751&hash=cktm0h6ur0o9006gsgf4cend8705evdm' type='video/mkv'>\
+            </video>\
         ";
     }
 
