284c56305bfb54a6937947c8af2dffca82e68cee
drosatto <danielrosatto@gmail.com>
2021-08-22 20:10:26 -0300

    prueba de video 7

--- js/providerMedia.js
+++ js/providerMedia.js
@@ -4,7 +4,7 @@ class ProviderMedia{
             <video\
                 width= '600' \
                 height= '480' >\
-                <source src='https://drive.google.com/file/d/1b4A0x3maDQ46Dv3MQO1fGz2NBokK9Apw/preview' type='video/mkv'>\
+                <source src='https://drive.google.com/uc?export=download&id=https://drive.google.com/file/d/1b4A0x3maDQ46Dv3MQO1fGz2NBokK9Apw/view?usp=sharing' type='video/mkv'>\
             </video>\
         ";
     }
