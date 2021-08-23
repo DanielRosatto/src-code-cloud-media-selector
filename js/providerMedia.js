35677ed75a213feda480e301652cb1bfa951a26d
drosatto <danielrosatto@gmail.com>
2021-08-22 21:10:51 -0300

    prueba de video 14

--- js/providerMedia.js
+++ js/providerMedia.js
@@ -1,8 +1,8 @@
 class ProviderMedia{
     constructor(){
         this.media_content="\
-            <video width= '600' height= '480' >\
-                <source src='https://drive.google.com/uc?export=preview&id=1ENjHRO4Eu-xH5Fjy3LfXLEfHweCz-yBR'\
+            <video width='600' height='480' controls autoplay >\
+                <source src='https://drive.google.com/uc?export=view&id=1ENjHRO4Eu-xH5Fjy3LfXLEfHweCz-yBR'\
                 type='video/mp4'>\
             </video>\
         ";
