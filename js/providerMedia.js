83868d6e61a4bb4517bede284af479e0a45c5b5e
drosatto <danielrosatto@gmail.com>
2021-08-23 08:43:34 -0300

    Video funcionando ok, se aplica estilo

--- js/providerMedia.js
+++ js/providerMedia.js
@@ -1,8 +1,9 @@
 class ProviderMedia{
     constructor(){
         this.media_content="\
-            <video width='600' height='480' controls autoplay >\
-                <source src='https://drive.google.com/uc?export=view&id=1ENjHRO4Eu-xH5Fjy3LfXLEfHweCz-yBR'\
+            <video class='col-9' height='75%' controls autoplay >\
+                <source src='https://drive.google.com/uc?export=view&id=\
+                1ENjHRO4Eu-xH5Fjy3LfXLEfHweCz-yBR'\
                 type='video/mp4'>\
             </video>\
         ";
