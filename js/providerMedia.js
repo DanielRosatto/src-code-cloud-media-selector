2de2b8a2a23e8b7b7166e36031997a580d0ab0c2
drosatto <danielrosatto@gmail.com>
2021-08-23 08:53:53 -0300

    Video funcionando ok, se corrige link

--- js/providerMedia.js
+++ js/providerMedia.js
@@ -2,8 +2,7 @@ class ProviderMedia{
     constructor(){
         this.media_content="\
             <video class='col-9' height='75%' controls autoplay >\
-                <source src='https://drive.google.com/uc?export=view&id=\
-                1ENjHRO4Eu-xH5Fjy3LfXLEfHweCz-yBR'\
+                <source src='https://drive.google.com/uc?export=view&id=1ENjHRO4Eu-xH5Fjy3LfXLEfHweCz-yBR'\
                 type='video/mp4'>\
             </video>\
         ";
