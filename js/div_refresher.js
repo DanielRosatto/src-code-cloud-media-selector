dc4941b34f1243703dc51a5cc5dbc2656607e1ff
drosatto <danielrosatto@gmail.com>
2021-08-24 16:49:28 -0300

    Prueba de estilo nro 3

--- js/div_refresher.js
+++ js/div_refresher.js
@@ -1,7 +1,7 @@
 class DivRefresher{
     constructor(elem){
         this.codeout=elem;
-        $(this.codeout).replaceWith("<div id='resultado'><h1>Hello, World!</h1></div>");
+        /* $(this.codeout).replaceWith("<div id='resultado'><h1>Hello, World!</h1></div>"); */
     }
 
     changeFor(code){
