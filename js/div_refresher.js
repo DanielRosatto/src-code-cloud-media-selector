ce5ff1673f97ac665ac22aa47b6e4be382ad1d2d
drosatto <danielrosatto@gmail.com>
2021-08-24 21:51:07 -0300

    Prueba de nav funcional nro 1

--- js/div_refresher.js
+++ js/div_refresher.js
@@ -1,3 +1,11 @@
+/**********************************************************************************************************
+ * Universidad Nacional Arturo Jauretche 2021                                                             *
+ * Cloud Media Selector                                                                                   *
+ * Autor: Rosatto Daniel                                                                                  *
+ * Prueba para Proyecto Animaciones                                                                       *
+ * Laboraorio 215                                                                                         *
+ * Archivo div_refresher.js                                                                                     *
+ **********************************************************************************************************/
 class DivRefresher{
     constructor(elem){
         this.codeout=elem;
--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@
   * Autor: Rosatto Daniel                                                                                  *
   * Prueba para Proyecto Animaciones                                                                       *
   * Laboraorio 215                                                                                         *
-  * Archivo main.js                                                                                     *
+  * Archivo main.js                                                                                        *
   **********************************************************************************************************/
 $(document).ready(function main(){
       refresher=new DivRefresher(
         "html body section#cont1.col-9 div.resultado"
         );
-      refresher.changeFor("../templates/Video2.html");
+
       refresher.changeFor("../templates/Video4.html");
+      $('.btn-video4').click(refresher.changeFor(refresher.changeFor("../templates/Video4.html")));
+      $('.btn-video3').click(refresher.changeFor(refresher.changeFor("../templates/Video3.html")));
+      $('.btn-video2').click(refresher.changeFor(refresher.changeFor("../templates/Video2.html")));
+      $('.btn-video1').click(refresher.changeFor(refresher.changeFor("../templates/Video1.html")));
     }
   );
