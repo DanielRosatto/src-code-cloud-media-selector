869a18bf8f4c46a92958bc1d6b0d8cc3e7362501
drosatto <danielrosatto@gmail.com>
2021-09-05 19:36:23 -0300

    se separa código fuente, se prueba 4

--- js/play_monitor.js
+++ js/play_monitor.js
@@ -11,5 +11,17 @@ $(document).ready(function main(document){
     });
 
     console.log('después de document.ready');
+    
+    // Get the video element with id="myVideo"
+    var vid = document.getElementById("myVideo");
+
+    // Assign an ontimeupdate event to the video element, and execute a function if the current playback position has changed
+    vid.ontimeupdate = function() {myFunction()};
+    
+    function myFunction() {
+      // Display the current position of the video in a p element with id="demo"
+      document.getElementById("demo").innerHTML = vid.currentTime;
+    }
+    
   }
 );
