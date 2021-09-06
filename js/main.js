f317a2674e463e968a65b4034724ee217bdb3af6
drosatto <danielrosatto@gmail.com>
2021-09-06 19:38:22 -0300

    se separa código fuente, se prueba 26

--- js/main.js
+++ js/main.js
@@ -9,6 +9,20 @@
   **********************************************************************************************************/
   $(document).ready(function main(){
     
+    var playVideo = document.getElementById("founded-show-bot");
+
+    console.log('después de document.ready '+playVideo.currentTime);
+
+    // Assign an ontimeupdate event to the video element, and execute a function if the current playback position has changed
+    playVideo.ontimeupdate = function() {myFunction()};
+
+    function myFunction() {
+      // Display the current position of the video in a p element with id="demo"
+      $("#widget-show-bot").empty();
+      $("#widget-show-bot").text(playVideo.currentTime);
+      console.log('en ontimeupdate: '+playVideo.currentTime);
+    }
+
     refresher = new DivRefresher(
         "html body section#cont1.col-9 div.resultado"
         );
@@ -24,19 +38,5 @@
           "../templates/Video4.html"
         ]
       );
-
-      var playVideo = document.getElementById("founded-show-bot");
-
-      console.log('después de document.ready '+playVideo.currentTime);
-
-      // Assign an ontimeupdate event to the video element, and execute a function if the current playback position has changed
-      playVideo.ontimeupdate = function() {myFunction()};
-
-      function myFunction() {
-        // Display the current position of the video in a p element with id="demo"
-        $("#widget-show-bot").empty();
-        $("#widget-show-bot").text(playVideo.currentTime);
-        console.log('en ontimeupdate: '+playVideo.currentTime);
-      }
     }
   );
