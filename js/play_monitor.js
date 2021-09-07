7b164632e3290b4555b4eb33273784da50ba9ce5
drosatto <danielrosatto@gmail.com>
2021-09-07 08:36:22 -0300

    se separa código fuente, prueba de refactor

--- js/play_monitor.js
+++ js/play_monitor.js
@@ -1,16 +1,11 @@
-$(document).ready(function main(){
-
-      function myFunction() {
-        // Display the current position of the video in a p element with id="demo"
-        $("#demo").empty();
-        $("#demo").text(x.currentTime);
-        console.log('en ontimeupdate: '+x.currentTime);
-      }
-
-      // Get the video element with id="myVideo"
-      var x = $("#myVideo")[0];
-
-      // Attach a timeupdate event to the video element, and execute a function if the current playback position has changed
-      x.addEventListener("timeupdate", myFunction);
-    }
-  );
+$(document).ready(function(){
+  $("#myVideo").on(
+      "timeupdate", 
+      function(event){
+    onTrackedVideoFrame(this.currentTime, this.duration);
+  });
+});
+      
+function onTrackedVideoFrame(currentTime, duration){
+  $("#demo").text("tiempo: " + currentTime +"/" + duration); //Change #current to currentTime
+}
