88019f4d6ac9dde5ab862a8b82187d8d6e2f15fe
drosatto <danielrosatto@gmail.com>
2021-10-14 09:11:23 -0300

    Se implementa contenido relacionado variable en el tiempo

--- js/show_bot.js
+++ js/show_bot.js
@@ -28,67 +28,24 @@
             "timeupdate", (function(passedInElement) {
                 if(typeof(passedInElement) != undefined  && passedInElement != null){
                     return function(e) {
-                        //passedInElement.forEach(function eachRelated(related){
-                        let curr = Math.round(e.target.currentTime,0);
                         // console.debug("Antes de if:  ... ");
-                        if(into_range(curr, passedInElement) == 1 && 
+                        if(into_range(e.target.currentTime, passedInElement)[0] == 1 && 
                             $(e.target).parent().parent().find("#elem_init").html() == ''){
-                            console.debug("Si se cumple:... "+
-                                $("#elem_init").html()
-                                );
-                            $(e.target).parent().parent().append("<div id='elem_init'></div>");
+                            // console.debug("Si se cumple:... "+getIndexMatch(e.target.currentTime, passedInElement));
                             $(e.target).parent().parent().find("#elem_init").load(
                                 "../templates/show_bot_widget.html",
-                                function() {
-                                    // $(document).ready(
-                                        replaceValue (
-                                            'btn-show-bot1',
-                                            passedInElement[getIndexMatch(curr, passedInElement)][2]
-                                        // )
-                                    );
+                                function() { 
+                                    replaceValue ('#btn-show-bot1',passedInElement[
+                                        into_range(e.target.currentTime, passedInElement)[1]
+                                    ][2]);
                                 }
                             );
                         }
-                        else if(into_range(curr, passedInElement) == 0 && 
-                                $(e.target).parent().parent().find("#elem_init").length > 0 ){
+                        else if(into_range(e.target.currentTime, passedInElement)[0] == 0 && 
+                                $(e.target).parent().parent().find("#elem_init").html() != ''){
                             // console.debug("Si no cumple: ... ");
                             $(e.target).parent().parent().find("#elem_init").empty();
                         }      
-                        /* if (curr <= related[1] && curr >= related[0]){ 
-                        switch (true) {
-                            case (
-                                curr <= related[1] && curr > related[0]
-                                // && $(e.target).parent().parent().find("#elem_init").length == 0
-                                ):
-                                    $(e.target).parent().parent().append("<div id='elem_init'></div>");
-                                    $(e.target).parent().parent().find("#elem_init").load(
-                                        "../templates/show_bot_widget.html", 
-                                        (function(a,b) {
-                                            console.debug("Rango y elem_init.length < 1:" + $("#elem_init").html());
-                                            $("#elem_init").find(a).prop('value', b);
-                                        }) ('.btn-show-bot',related[2])
-                                    );
-                                break;
-                            case (curr < related[0]):
-                                // console.debug("Antes del rango: "+related+" ... ");
-                                break;
-                            case (curr == related[1] && $(e.target).parent().parent().find("#elem_init").length >= 1 ):
-                                $(e.target).parent().parent().find("#elem_init").remove();
-                                console.debug("Justo después del rango: "+related+" ... ");
-                                break;
-                            case (curr > related[1] && $(e.target).parent().parent().find("#elem_init").length == 0 ):
-                                console.debug("Después del rango: "+related+" ... ");
-                                break;
-                            default:
-                                console.debug(
-                                        "Error de parámetros: "+
-                                        $(e.target).parent().parent().find('#elem_init').html()+
-                                        " PassedIn: "+related
-                                    );
-                                break;
-                            } 
-                        }*/
-                    //});
                 }}}) (this.related_arr)
         );
     }
@@ -112,48 +69,6 @@
             $("<div id='deb1'><p>Inicio Debug:</p></div>").appendTo("#resultado");
         }
     }
-
-/*     init(){
-        $(this.video_to_listen).on(
-            "timeupdate", 
-            (function(passedInElement) {
-                return function(e) { 
-                    switch (true) {
-                        case (
-                            e.target.currentTime < passedInElement[0] &&
-                            e.target.currentTime > passedInElement[1]):
-                            console.debug("Dentro del rango");
-                            $(e.target).parent().parent().find('span').text(
-                                "tiempo: " + 
-                                e.target.currentTime +
-                                "/" + 
-                                e.target.duration +
-                                ", fadeout: "+ passedInElement[0] +
-                                ", time to suggest: "+passedInElement[1]
-                            ); 
-                            if($(e.target).parent().parent().find("#elem_init").length < 1){
-                                $(e.target).parent().parent().append("<div id='elem_init'></div>");
-                                $(e.target).parent().parent().find("#elem_init").load("../templates/show_bot_widget.html");
-                            }
-                            break;
-                        case (e.target.currentTime < passedInElement[0]):
-                            console.debug("Antes del rango");
-                            break;
-                        case (e.target.currentTime > passedInElement[1]):
-                            console.debug("Después del rango"); 
-                            if($(e.target).parent().parent().find("#elem_init").length > 0 ){
-                                $(e.target).parent().parent().find("#elem_init").remove();
-                            }
-                            break;
-                        default:
-                            console.debug("Desde event: "+
-                                $(e.target).parent().parent().find('span').html() );
-                            break;
-                    }
-                }
-            }) ([this.time_fadeout, this.start_time_show_suggest])
-        );
-    } */
 }
 
 
@@ -170,31 +85,26 @@ function compareRelatedArr(a, b) {
     // salida si los tiempos no son lógicos
     return undefined;
 }
-  
+
 function into_range(current, arr){
-    let rtn = 0;
-    arr.forEach(function eachInArr(related){
-        if (related[0]<=current && current<=related[1]){
-            rtn = 1;
+    // let curr=Math.round(current,0);
+    let rtn = [0,0];
+    arr.forEach(function eachIndex(related,index){
+        // console.debug(
+        // "[get index]: [0]="+related[0]+
+        // " [1]="+related[1]+
+        // " Curr= "+curr +
+        // " igualdad="+(related[0]<=curr && curr<=related[1])+
+        // " index= "+ index);
+        if ((related[0]<=current && current<=related[1])==true){
+            rtn = [1,index];
         }
-    });
-    if(rtn==0)console.debug("[Into Range]: out: "+rtn+" ... curr: "+current);
+     });
+    // console.debug("[get index]: out "+index+", curr: "+current+", i: "+i);
     return rtn;
 }
 
-function getIndexMatch(current, arr){
-    let i=0;
-    arr.forEach(function eachIndex(related){
-        if (related[0]<=current && current<=related[1]){
-            return i;
-        }
-        i=i+1;
-    });
-    console.debug("[get index]: out "+i+", curr: "+current+", i: "+i);
-    return i;
-}
-
 function replaceValue(a,b) {
-    console.debug("A:"+ a +" B: " + b + $("#elem_init").html());
-    document.getElementById(a).setAttribute('value', b);
+    // console.debug("A:"+ a +" B: " + b + $("#elem_init").html());
+    $(a).prop('value', b);
 }
