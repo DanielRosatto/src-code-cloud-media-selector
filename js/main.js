9e9092859ed85e2de6be874ebd765f69cdf1a806
drosatto <danielrosatto@gmail.com>
2021-10-27 21:46:35 -0300

    Se implementa redireccionador, link corregido

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@
         /*id del video*/"#mediaShowBot",
         [/*Lista del contenido relacionado: Intervalo, link, indicación corta, descripción*/
           /**/[5,10,"https://www.buenosaires.gob.ar", "Indicación 1", "Una descripción para el elemento 1."],
-          /**/[26,30,"cloud-media-selector.herokuapp.com?param1=Video_5", "Ir a Video 5", "Una descripción para el elemento A."],
+          /**/[26,30,"https://cloud-media-selector.herokuapp.com/?param1=Video_5", "Ir a Video 5", "Una descripción para el elemento A."],
           /**/[36,44,"https://fiuba.edu.ar", "Indicación b", "Una descripción para el elemento b."],
           /**/[13,18,"unq.edu.ar", "Indicación 3", "Una descripción para el elemento 3."]
         ]
